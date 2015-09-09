494f0d7bd2bb2ec50307c290bc7d05b930a6ad48
Luke Duncan <luke.will.duncan@gmail.com>
2015-09-09 15:26:55 -0500

    first winner if statement made and working

--- js/game.js
+++ js/game.js
@@ -5,12 +5,12 @@ $(function(){
 	var pieceX = "X";
 	var pieceO = "O";
 
-	var whosTurn = 0;
+	var switchTurn = 0;
 
 
     // setting who's turn it is -- basing it off count 0. Even = pieceX, Odd = pieceO
       $(".cell-design").click(function() { 
-            if (whosTurn % 2 === 0){
+            if (switchTurn % 2 === 0){
               $(this).text(pieceX);
               findWinner(); 
             } else {
@@ -18,20 +18,32 @@ $(function(){
               $(this).text(pieceO);
               findWinner();
             }
-          whosTurn++; 
+          switchTurn++; 
       });
 
     // find the winnner -- function for searching winner for pieceX and pieceO
-    // comment -- originally was thinking of creating 2 functions, one to find if pieceX won, other to find out if pieceO won. Using the .text without declaring piece, works instead.
     function findWinner() {
     	//checks to see if x has won the game
 
-    	if ($("#cell0").text() == $("#cell1").text()) {
-			if ($("#cell0").text() == $("#cell2").text()) {
-				alert("Game Over. Player " + $("#cell0").text() + " wins!")
+    	var searchCell0 = $("#cell0").text() !== " ";
+
+    	if (searchCell0) {
+	    	if ($("#cell0").text() == $("#cell1").text()) {
+				if ($("#cell0").text() == $("#cell2").text()) {
+					alert("Game Over. Player " + $("#cell0").text() + " wins!")
+				}
+			}
+			if ($("#cell0").text() == $("#cell3").text()) {
+				if ($("#cell0").text() == $("#cell6").text()) {
+					alert("Game Over. Player " + $("#cell0").text() + " wins!")
+				}
+			}
+			if ($("#cell0").text() == $("#cell4").text()) {
+				if ($("#cell0").text() == $("#cell8").text()) {
+					alert("Game Over. Player " + $("#cell0").text() + " wins!")
+				}
 			}
 		}
-
 
 	};
 
