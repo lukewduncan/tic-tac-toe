db0da7a010ae85523ff3111ce7ed5b65ae6c52ed
Luke Duncan <luke.will.duncan@gmail.com>
2015-09-09 16:19:59 -0500

    added findWinner function to global cell-design event-listener and created vars for cell ids

--- js/game.js
+++ js/game.js
@@ -1,51 +1,60 @@
 $(function(){
 
-	
-
 	var pieceX = "X";
 	var pieceO = "O";
 
+	// took concept from the ATM assignment
 	var switchTurn = 0;
 
 
     // setting who's turn it is -- basing it off count 0. Even = pieceX, Odd = pieceO
       $(".cell-design").click(function() { 
-            if (switchTurn % 2 === 0){
-              $(this).text(pieceX);
-              findWinner(); 
-            } else {
-            	// if player move doesn't equal even number, then pieceO move
-              $(this).text(pieceO);
-              findWinner();
-            }
-          switchTurn++; 
+	            if (switchTurn % 2 === 0){
+	              		$(this).text(pieceX);
+	            } else {
+	            	// if player move doesn't equal even number, then pieceO turn
+	              $(this).text(pieceO);
+	            }
+	        switchTurn++; 
+	        findWinner();
       });
 
     // find the winnner -- function for searching winner for pieceX and pieceO
     function findWinner() {
     	//checks to see if x has won the game
 
+    	var cellzero = $("#cell0");
+    	var cellone = $("#cell1");
+    	var celltwo = $("#cell2");
+    	var cellthree = $("#cell3");
+    	var cellfour = $("#cell4");
+    	var cellfive = $("#cell5");
+    	var cellsix = $("#cell6");
+    	var cellseven = $("#cell7")
+    	var celleight = $("#cell8");
+    	var cellnine = $("cell9");
+
     	var searchCell0 = $("#cell0").text() !== " ";
 
     	if (searchCell0) {
-	    	if ($("#cell0").text() == $("#cell1").text()) {
-				if ($("#cell0").text() == $("#cell2").text()) {
-					alert("Game Over. Player " + $("#cell0").text() + " wins!")
+	    	if ($(cellzero).text() === $(cellone).text()) {
+				if ($(cellzero).text() === $(celltwo).text()) {
+					alert("Game Over. Player " + $(cellzero).text() + " wins!")
 				}
 			}
-			if ($("#cell0").text() == $("#cell3").text()) {
-				if ($("#cell0").text() == $("#cell6").text()) {
-					alert("Game Over. Player " + $("#cell0").text() + " wins!")
+			if ($(cellzero).text() === $(cellthree).text()) {
+				if ($(cellzero).text() === $(cellsix).text()) {
+					alert("Game Over. Player " + $(cellzero).text() + " wins!")
 				}
 			}
-			if ($("#cell0").text() == $("#cell4").text()) {
-				if ($("#cell0").text() == $("#cell8").text()) {
-					alert("Game Over. Player " + $("#cell0").text() + " wins!")
+			if ($(cellzero).text() === $(cellfour).text()) {
+				if ($(cellzero).text() === $(celleight).text()) {
+					alert("Game Over. Player " + $("cellzero").text() + " wins!")
 				}
 			}
-		}
+		} //end of first row (all possible combinations accounted for from cellzero)
 
-	};
+	}; // end of findWinner function
 
 }); 
 
