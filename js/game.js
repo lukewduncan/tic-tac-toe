796b1e646976fc224906eb1d2ff6efbe8d427b60
Luke Duncan <luke.will.duncan@gmail.com>
2015-09-10 09:49:49 -0500

    added in all winning functions

--- js/game.js
+++ js/game.js
@@ -6,8 +6,6 @@ $(function() {
 
     var switchTurn = 0;
 
-    alert("Player 1 - make your move!")
-
     // Turn logic -- setting who's turn it is -- basing it off count 0 (ATM assignment). Even = pieceX, Odd = pieceO
     $(cellDefault).click(function() {
         switchTurn++;
@@ -17,8 +15,10 @@ $(function() {
             // if player move doesn't equal even number, then pieceO turn
             $(this).text(pieceO);
         }
+
         console.log("switch turn testing");
 
+    
         findWinner();
     });
 
@@ -36,8 +36,15 @@ $(function() {
         var celleight = $("#cell8");
         var cellnine = $("cell9");
 
+        // combos
+        // 1 - [0,1,2] [0,4,8] [0,3,6]
+        // 2 - [2,4,6] [2,5,8]
+        // 3 - [1,4,7]
+        // 4 - [3,4,5]
+        // 5 - [6,7,8]
 
-        // first 3 solutions to win
+        // 1
+        // var for if cellzero has text
         var searchCell0 = $(cellzero).text() !== " ";
 
         if (searchCell0) {
@@ -57,7 +64,56 @@ $(function() {
                     alert("Game Over. Player " + $(cellzero).text() + " wins!");
                 }
             }
-        } //end of first row (all possible combinations accounted for from cellzero)
+        }
+
+        // 2
+        var searchCell2 = $(celltwo).text() !==" ";
+
+        if (searchCell2) {
+        	if ($(celltwo).text() === $ (cellfive).text()) {
+        		if ($(celltwo).text() === $(celleight).text()) {
+                    alert("Game Over. Player " + $(celltwo).text() + " wins!");
+	            }
+        	}
+        	if ($(celltwo).text() === $ (cellfour).text()){
+        		if ($(celltwo).text() === $(cellsix).text()) {
+                    alert("Game Over. Player " + $(celltwo).text() + " wins!");
+	            }
+        	}
+        }
+
+        // 3
+        var searchCell1 = $(cellone).text() !==" ";
+
+        if (searchCell1) {
+        	if ($(cellone).text() === $ (cellfour).text()) {
+        		if ($(cellone).text() === $(cellseven).text()) {
+                    alert("Game Over. Player " + $(cellone).text() + " wins!");
+	            }
+        	}
+        }
+
+        // 4
+        var searchCell3 = $(cellthree).text() !==" ";
+
+        if (searchCell3) {
+        	if ($(cellthree).text() === $ (cellfour).text()) {
+        		if ($(cellthree).text() === $(cellfour).text()) {
+                    alert("Game Over. Player " + $(cellthree).text() + " wins!");
+	            }
+        	}
+        }
+
+        // 5
+        var searchCell6 = $(cellsix).text() !==" ";
+
+        if (searchCell6) {
+        	if ($(cellsix).text() === $ (cellseven).text()) {
+        		if ($(cellsix).text() === $(celleight).text()) {
+                    alert("Game Over. Player " + $(cellsix).text() + " wins!");
+	            }
+        	}
+        }
     }; // end of findWinner function
 
     refreshGame();
@@ -68,4 +124,4 @@ $(function() {
         }
     };
 
-});
+}); // end of on.document load
