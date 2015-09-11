0e56135fd7d1df720c0abc2578587e34e8383769
Luke Duncan <luke.will.duncan@gmail.com>
2015-09-11 12:50:02 -0500

    created move counter

--- js/game.js
+++ js/game.js
@@ -8,48 +8,51 @@ $(function() {
 
     // Turn logic -- setting who's turn it is -- basing it off count 0 (ATM assignment). Even = pieceX, Odd = pieceO
     $(".cell-default").click(function() {
-    	        switchTurn++;
+        switchTurn++;
 
         // denies a move into a cell if taken
         if ($(this).hasClass("deny")) {
-        	alert("I spent 8 hours trying to block you from doing this. So you definitely aren't making this move!")
-        	return;
-        	switchTurn--;
+            alert("I spent 8 hours trying to block you from doing this. So you definitely aren't making this move!")
+            return;
+            switchTurn--;
         }
-
-	        if (playerTurn === 1) {
-	        	$(this).text(pieceX);
-	        	$(this).addClass("deny");
-	        	playerTurn = 2;
-	        } else if (playerTurn === 2) {
-	        	$(this).text(pieceO);
-	        	$(this).addClass("deny");
-	        	playerTurn = 1;
-	        }
-	    findWinner();
-        findTie();  
+        $('.score-count').html(function(i, val) {return val*1+1 });
+        if (playerTurn === 1) {
+            $(this).text(pieceX);
+            $(this).addClass("deny");
+            playerTurn = 2;
+        } else if (playerTurn === 2) {
+            $(this).text(pieceO);
+            $(this).addClass("deny");
+            playerTurn = 1;
+        }
+        findWinner();
+        findTie();
     });
 
 
+
     // Function for showing alert before the start of every game
-	function alertStart () {
-		if (playerTurn === 1) {
-			alert("PLAYER 1, IT'S YOUR TURN FIRST! YOU ARE PIECE X");
-		}
-		else {
-			alert("PLAYER 2, IT'S YOUR TURN FIRST! YOU ARE piece O");
-		}
-	};
+    function alertStart() {
+        if (playerTurn === 1) {
+            alert("PLAYER 1, IT'S YOUR TURN FIRST! YOU ARE X");
+        } else {
+            alert("PLAYER 2, IT'S YOUR TURN FIRST! YOU ARE O");
+        }
+    };
 
     // Tie function
     function findTie() {
-    	if (switchTurn === 9) {
-    		($(".history-display").append("<li>" + "It was a tie!" + "</li>"));
-        	alert('You tied. Play again!');
-    		refreshGame();
-    		switchTurn = 0;
-    	}
+        if (switchTurn === 9) {
+            ($(".history-display").append("<li>" + "It was a tie!" + "</li>"));
+            alert('YOU TIED. PLAY AGAIN!');
+            refreshGame();
+            switchTurn = 0;
+        }
     };
+
+    $('.cell-default').click(function() {
+	});
 
     // Refresh Game after win, tie or loss
     function refreshGame() {
@@ -87,7 +90,7 @@ $(function() {
         if (searchCell0) {
             if ($(cellzero).text() === $(cellone).text()) {
                 if ($(cellzero).text() === $(celltwo).text()) {
-                	($(".history-display").append("<li>" + cellzero.text() + " wins!</li>"));
+                    ($(".history-display").append("<li>" + cellzero.text() + " wins!</li>"));
                     alert("Game Over. Player " + $(cellzero).text() + " wins!");
                     refreshGame();
                     return;
@@ -95,7 +98,7 @@ $(function() {
             }
             if ($(cellzero).text() === $(cellthree).text()) {
                 if ($(cellzero).text() === $(cellsix).text()) {
-                	($(".history-display").append("<li>" + cellzero.text() + " wins!</li>"));
+                    ($(".history-display").append("<li>" + cellzero.text() + " wins!</li>"));
                     alert("Game Over. Player " + $(cellzero).text() + " wins!");
                     refreshGame();
                     return;
@@ -103,7 +106,7 @@ $(function() {
             }
             if ($(cellzero).text() === $(cellfour).text()) {
                 if ($(cellzero).text() === $(celleight).text()) {
-                	($(".history-display").append("<li>" + cellzero.text() + " wins!</li>"));
+                    ($(".history-display").append("<li>" + cellzero.text() + " wins!</li>"));
                     alert("Game Over. Player " + $(cellzero).text() + " wins!");
                     refreshGame();
                 }
@@ -111,62 +114,62 @@ $(function() {
         }
 
         // 2
-        var searchCell2 = $(celltwo).text() !==" ";
+        var searchCell2 = $(celltwo).text() !== " ";
 
         if (searchCell2) {
-        	if ($(celltwo).text() === $ (cellfive).text()) {
-        		if ($(celltwo).text() === $(celleight).text()) {
-        			($(".history-display").append("<li>" + celltwo.text() + " wins!</li>"));
+            if ($(celltwo).text() === $(cellfive).text()) {
+                if ($(celltwo).text() === $(celleight).text()) {
+                    ($(".history-display").append("<li>" + celltwo.text() + " wins!</li>"));
                     alert("Game Over. Player " + $(celltwo).text() + " wins!");
                     refreshGame();
-	            }
-        	}
-        	if ($(celltwo).text() === $ (cellfour).text()){
-        		if ($(celltwo).text() === $(cellsix).text()) {
-        			($(".history-display").append("<li>" + celltwo.text() + " wins!</li>"));
+                }
+            }
+            if ($(celltwo).text() === $(cellfour).text()) {
+                if ($(celltwo).text() === $(cellsix).text()) {
+                    ($(".history-display").append("<li>" + celltwo.text() + " wins!</li>"));
                     alert("Game Over. Player " + $(celltwo).text() + " wins!");
                     refreshGame();
-	            }
-        	}
+                }
+            }
         }
 
         // 3
-        var searchCell1 = $(cellone).text() !==" ";
+        var searchCell1 = $(cellone).text() !== " ";
 
         if (searchCell1) {
-        	if ($(cellone).text() === $ (cellfour).text()) {
-        		if ($(cellone).text() === $(cellseven).text()) {
-        			($(".history-display").append("<li>" + cellone.text() + " wins!</li>"));
+            if ($(cellone).text() === $(cellfour).text()) {
+                if ($(cellone).text() === $(cellseven).text()) {
+                    ($(".history-display").append("<li>" + cellone.text() + " wins!</li>"));
                     alert("Game Over. Player " + $(cellone).text() + " wins!");
                     refreshGame();
-	            }
-        	}
+                }
+            }
         }
 
         // 4
-        var searchCell3 = $(cellthree).text() !==" ";
+        var searchCell3 = $(cellthree).text() !== " ";
 
         if (searchCell3) {
-        	if ($(cellthree).text() === $ (cellfour).text()) {
-        		if ($(cellthree).text() === $(cellfive).text()) {
-        			($(".history-display").append("<li>" + cellthree.text() + " wins!</li>"));
+            if ($(cellthree).text() === $(cellfour).text()) {
+                if ($(cellthree).text() === $(cellfive).text()) {
+                    ($(".history-display").append("<li>" + cellthree.text() + " wins!</li>"));
                     alert("Game Over. Player " + $(cellthree).text() + " wins!");
                     refreshGame();
-	            }
-        	}
+                }
+            }
         }
 
         // 5
-        var searchCell6 = $(cellsix).text() !==" ";
+        var searchCell6 = $(cellsix).text() !== " ";
 
         if (searchCell6) {
-        	if ($(cellsix).text() === $ (cellseven).text()) {
-        		if ($(cellsix).text() === $(celleight).text()) {
-        			($(".history-display").append("<li>" + cellsix.text() + " wins!</li>"));
+            if ($(cellsix).text() === $(cellseven).text()) {
+                if ($(cellsix).text() === $(celleight).text()) {
+                    ($(".history-display").append("<li>" + cellsix.text() + " wins!</li>"));
                     alert("Game Over. Player " + $(cellsix).text() + " wins!");
                     refreshGame();
-	            }
-        	}
+                }
+            }
         }
     }; // end of findWinner function
 
